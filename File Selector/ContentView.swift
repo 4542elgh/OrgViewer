5aa8a01b9c108f3ccd5d74699e4ae95041fb551a
4542elgh <4542elgh@gmail.com>
2021-03-16 01:20:21 -0700

    Initial Commit

---
+++ File Selector/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  File Selector
+//
+//  Created by Evan Liu on 3/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ File Selector/File_SelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  File_SelectorApp.swift
+//  File Selector
+//
+//  Created by Evan Liu on 3/16/21.
+//
+
+import SwiftUI
+
+@main
+struct File_SelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
